9eb252784e18d1ada11e01d108a10800eb5cf085
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-06-22 02:16:38 +0200

    Laod and display rocket image from skimage

--- intro.py
+++ intro.py
@@ -1,4 +1,26 @@
 
 #Author: Mangaliso M Mngomezulu
 #desc: Datacamp image processing course
-#here we go!
+
+from skimage import data
+from skimage import color
+import matplotlib.pyplot as plt
+
+#load rocket image, found in the library
+rocket_image = data.rocket()
+
+#covert the image to grayscale
+#grayscale = color.rgb2gray(original)
+
+#convert grayscale to rgb
+#rgb = color.gray2rgb(grayscale)
+
+#the show image function
+def show_image(image, title='Image', cmap_type='gray'):
+    plt.imshow(image, cmap=cmap_type)
+    plt.title(title)
+    plt.axis('off')
+    plt.show()
+
+#display the image of a rocket
+show_image(rocket_image,"Rocket Image")
