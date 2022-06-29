a0ae49ac7eb8eb0d17d7922b401e324019f390f4
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-06-30 00:01:43 +0200

    Updated custom lib

--- custom_lib.py
+++ custom_lib.py
@@ -17,3 +17,11 @@ def show_image(image, title='Image', cmap_type='gray'):
     plt.title(title)
     plt.axis('off')
     plt.show()
+
+
+def show_image_with_detected_corners(image, coords, title="Corners detected"):
+    plt.imshow(image, interpolation='nearest', cmap='gray')
+    plt.title(title)
+    plt.plot(coords[:,1], coords[:, 0], '+r', markersize=15)
+    plt.axis('off')
+    plt.show()
