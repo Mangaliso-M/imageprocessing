62f1bd613f4953a7f32fecb02d2ae7a2ee36ca84
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-07-06 21:26:13 +0200

    if _name_== '_main_': execution control

--- biomedical_image_analysis/n_dimensional_images.py
+++ biomedical_image_analysis/n_dimensional_images.py
@@ -15,14 +15,16 @@ print("The shape for image 2 is: ",image2.shape) #(512, 512)
 
 volmetric_image = np.stack([image1, image2, image3]) 
 
-#(3, 512, 512)
-print("The shape for the volmetric image data is:", volmetric_image.shape)
-
-#imageio.volread() reads data as a volume directly from disk, takes as an arg the folder name
-print("directory files: ",os.listdir('./datasets/tcia-chest-ct-sample/'))
 
 #read the directory as one volmetric data for processing,uses metadata to sort the list, if metadata is not available it defaults to ...
 vol = imageio.volread('./datasets/tcia-chest-ct-sample/')
+
+if __name__ == "_main_":
+    #(3, 512, 512)
+    print("The shape for the volmetric image data is:", volmetric_image.shape)
+
+    #imageio.volread() reads data as a volume directly from disk, takes as an arg the folder name
+    print("directory files: ",os.listdir('./datasets/tcia-chest-ct-sample/'))
 
 #the metadata from the volume can be unpacked 
 #im1, im2, im3 = vol.meta['attribute']
