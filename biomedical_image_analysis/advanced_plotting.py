7e3ec0f12ec9dc7cce0bcaeacc111f36b9e23596
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-07-06 22:17:30 +0200

    Aspect ratio controlled volmetric data plotting

--- biomedical_image_analysis/advanced_plotting.py
+++ biomedical_image_analysis/advanced_plotting.py
@@ -33,4 +33,19 @@ view_c = vol[ :, :0] #one image (1st vs 2nd)
 #plt.show()
 
 #modifying the aspect ratio
-#it happens that the images donot have the same dimensions, yet we want to stack them. So the procedure is to stretch the pixels on one side so that the images can be compatible fo
+#it happens that the images donot have the same dimensions, yet we want to stack them. So the procedure is to stretch the pixels on one side so that the images can be compatible for combined plotting
+#the amount of stretching needed is dertermined by the aspect ratio
+
+image= vol[:, :, 100] #data from the first and second dimensions
+d0,d1,d2 = vol.meta['sampling']
+print(d0, d1, d2) #3.269999999999996 0.976562 0.976562
+
+aspect_ratio = d0 / d1
+
+print("The aspect ratio is", aspect_ratio) #The aspect ratio is 3.3484817144226335
+
+#plotting the final image requires that the aspect ratio be speficied, it will help in determininng the strech to meet the unequalness of the image dimensions.
+#failure to adjust the aspect results in a distorted image.
+plt.imshow(image, cmap='gray', aspect=aspect_ratio)
+plt.title("Aspect ratio controlled volmetric data plotting")
+plt.show()
