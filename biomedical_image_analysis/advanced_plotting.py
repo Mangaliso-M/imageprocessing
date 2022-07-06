3b8a993e727d8e11b3ff9c134095ecabeedb3f02
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-07-06 21:34:04 +0200

    Plotting volmentric data

--- biomedical_image_analysis/advanced_plotting.py
+++ biomedical_image_analysis/advanced_plotting.py
@@ -3,3 +3,20 @@
 
 import imageio
 from n_dimensional_images import vol #from a custom file
+import matplotlib.pyplot as plt
+
+volumetric_data = vol
+
+fig, axes = plt.subplots(nrows=1, ncols=3) # a display with 3 columns to show images, in one row
+
+#display the three slices on the figure's axis
+axes[0].imshow(volumetric_data[0], cmap='gray')
+axes[1].imshow(volumetric_data[1], cmap='gray')
+axes[2].imshow(volumetric_data[2], cmap='gray')
+
+#remove the axis for each of the three axes
+for ax in axes: 
+    ax.axis('off')
+
+#now display the figure
+plt.show()
