31104a08ad5cd803f6223b72153f79e9f078ca74
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-07-16 23:40:32 +0200

    Read and stack volmetric image data

--- biomedical_image_analysis/image_comparison/resampling_and_interpolation.py
+++ biomedical_image_analysis/image_comparison/resampling_and_interpolation.py
@@ -5,5 +5,15 @@ import imageio
 import matplotlib.pyplot as plt
 import numpy as np
 
-im = imageio.imread('././datasets/OASISsample/healthybrainscan.jpg')
+#one image is used here owing to the absence of the main dataset, this is just meant to understand the code
+#resampling increases or decreases the amount of space occupied by each pixel - its diffrent from croppin
+im1 = imageio.imread('././datasets/OASISsample/healthybrainscan.jpg')
+im2 = imageio.imread('././datasets/OASISsample/healthybrainscan.jpg')
+im3 = imageio.imread('././datasets/OASISsample/healthybrainscan.jpg')
+
+vol = np.stack([im1, im2, im3])
+
+for image in vol:
+    plt.imshow(image)
+    plt.show()
 
