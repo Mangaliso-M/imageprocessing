d8265adf3b7732c5138fa386951a0efc1556aa78
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-07-16 19:50:36 +0200

    load image

---
+++ biomedical_image_analysis/image_comparison/resampling_and_interpolation.py
@@ -0,0 +1,9 @@
+#@author: Mangaliso M. Mngomezulu
+#description: image resampling and interpolation
+
+import imageio
+import matplotlib.pyplot as plt
+import numpy as np
+
+im = imageio.imread('././datasets/OASISsample/healthybrainscan.jpg')
+
