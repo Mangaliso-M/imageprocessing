a7d6e27795477e023913e217d354f0d9bbd4240c
Mangaliso-M <mangalisommngomezulu@gmail.com>
2022-07-17 14:45:24 +0200

    Resampling and Interpolation

--- biomedical_image_analysis/image_comparison/resampling_and_interpolation.py
+++ biomedical_image_analysis/image_comparison/resampling_and_interpolation.py
@@ -4,6 +4,7 @@
 import imageio
 import matplotlib.pyplot as plt
 import numpy as np
+import scipy.ndimage as ndi
 
 #one image is used here owing to the absence of the main dataset, this is just meant to understand the code
 #resampling increases or decreases the amount of space occupied by each pixel - its diffrent from croppin
@@ -13,7 +14,12 @@ im3 = imageio.imread('././datasets/OASISsample/healthybrainscan.jpg')
 
 vol = np.stack([im1, im2, im3])
 
-for image in vol:
-    plt.imshow(image)
-    plt.show()
+print("Volume shape:",vol.shape)
 
+#zoom param = 2 means we double the width of each axis
+print("Zoomed volume shape:", ndi.zoom(vol, zoom=2).shape)
+
+"""
+Resampling creates a new image based on the old one. Filling up this image requires 
+a process refered to as interpolation.
+"""
